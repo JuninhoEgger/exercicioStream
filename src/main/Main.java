a1d35388dc792b05752fc37aa2ca7d165290f521
Osvaldo Egger Junior <junior.egger@gmail.com>
2022-05-31 10:20:09 -0300

    primeira parte do exercício

---
+++ src/main/Main.java
@@ -0,0 +1,48 @@
+package main;
+
+import person.Person;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.String.format;
+import static java.util.stream.Collectors.toList;
+import static javax.swing.JOptionPane.showInputDialog;
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Person> people = new ArrayList<>();
+        List<Person> emails;
+        List<Person> peopleSum;
+        Double sum = 0.0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\estudos\\people.txt"))) {
+
+            String line = br.readLine();
+            Double salary = parseDouble(showInputDialog("ENTER SALARY:"));
+
+            while (line != null) {
+                String[] fields = line.split(",");
+                people.add(new Person(fields[0], fields[1], parseDouble(fields[2])));
+                line = br.readLine();
+            }
+
+            emails = people.stream().filter(p -> p.getSalary() >= salary).collect(toList());
+            StringBuilder response = new StringBuilder("EMAIL OF PEOPLE WHOSE SALARY IS MORE THAN R$ " + format("%.2f", salary) + "\n");
+            emails.forEach(email -> response.append(email.getEmail()).append("\n"));
+            showMessageDialog(null, response);
+
+
+        } catch (IOException e) {
+            showMessageDialog(null, e.getMessage());
+        }
+
+    }
+}
